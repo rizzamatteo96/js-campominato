236e670ee61fa9a94365d14446dd72efb06d6dca
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-09 15:47:03 +0200

    add html code for game section adjust js for html

--- js/script.js
+++ js/script.js
@@ -8,51 +8,94 @@
 //TODO BONUS: (da fare solo se funziona tutto il resto) all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50
 
 
-//*BONUS Impostare livello di difficoltà del gioco
-var userLvl = parseInt(prompt('Inserisci la difficoltà: 0 / 1 / 2'));
-
-// Check se il numero inserito può essere accettato o meno dal sistema
-while(!checkRange(0,2,userLvl)){
-    alert('Attenzione!!!! Il dato inserito non è corretto.');
-    userLvl = parseInt(prompt('Inserisci la difficoltà. Il valore inserito DEVE essere uno dei seguenti: 0 / 1 / 2'));
-}
-
-switch (userLvl){
-    case 0:
-        //imposta valori per la difficoltà 0
-        var bombeMin = 1;
-        var bombeMax = 100;
-        break;
-    case 1:
-        //imposta valori per la difficoltà 1
-        var bombeMin = 1;
-        var bombeMax = 80;
-        break;
-    case 2:
-        //imposta valori per la difficoltà 2
-        var bombeMin = 1;
-        var bombeMax = 50;
-        break;
-}
-
-
-// 1. Generare i 16 numeri casuali che andranno a definire le "bombe"
+// dichiarazione delle variabili globali
+var bombeMin;
+var bombeMax;
 var bombe = [];
 var numeroBombe = 16;
-while (bombe.length < numeroBombe){
-    // genera un numero random
-    var numGen = rndNumber(bombeMin,bombeMax);
-    if(!bombe.includes(numGen)){
-        //controllo che il numero generato non sia già presente e lo inserisco nell'array di riferimento
-        bombe.push(numGen);
-    }
-}
-console.log(bombe);
-
-
-// 2. Richiedere all'utente un numero tra 1 e 100 e verificare che non sia già stato scritto
 var saveUserNum = [];
 var endGame = false;
+
+
+//*BONUS Impostare livello di difficoltà del gioco
+document.getElementById('btn-lvl').addEventListener('click', function(){
+    var userLvl = parseInt(document.getElementById('level').value);
+    console.log(userLvl);
+    // Check se il numero inserito può essere accettato o meno dal sistema
+    if(checkRange(0,2,userLvl)){
+        switch (userLvl){
+            case 0:
+                //imposta valori per la difficoltà 0
+                bombeMin = 1;
+                bombeMax = 100;
+                break;
+            case 1:
+                //imposta valori per la difficoltà 1
+                bombeMin = 1;
+                bombeMax = 80;
+                break;
+            case 2:
+                //imposta valori per la difficoltà 2
+                bombeMin = 1;
+                bombeMax = 50;
+                break;
+        }
+        console.log('min' + bombeMin);
+        console.log('max' + bombeMax);
+        console.log('step' + step);
+    } 
+    else{
+        alert('Attenzione!!!! Non hai inserito un livello coerente');
+        document.getElementById('level').value = '';
+    }
+
+    // 1. Generare i 16 numeri casuali che andranno a definire le "bombe"
+    while (bombe.length < numeroBombe){
+        // genera un numero random
+        var numGen = rndNumber(bombeMin,bombeMax);
+        if(!bombe.includes(numGen)){
+            //controllo che il numero generato non sia già presente e lo inserisco nell'array di riferimento
+            bombe.push(numGen);
+        }
+    }
+    console.log(bombe);
+
+    // faccio scomparire la sezione di selezione del livello e faccio apparire quella di gioco
+    document.getElementById('game-lvl').className = 'd-none';
+});
+
+
+// Dopo aver inserito un numero e aver premuto il tasto, il programma controlla le varie condizioni del caso per fare in modo di dare un allarme, inserire il numero o bloccare il gioco
+document.getElementById('btn-game').addEventListener('click', function(){
+    if(saveUserNum.length < bombeMax - numeroBombe && !endGame){
+        var numUser = parseInt(document.getElementById('user-number').value);
+        if (!bombe.includes(numUser) && !saveUserNum.includes(numUser) && !isNaN(numUser) && checkRange(bombeMin,bombeMax,numUser)){
+            //il numero è accettato dal sistema quindi lo salvo nell'array
+            saveUserNum.push(numUser);
+        }
+        else if(isNaN(numUser)){
+            //il dato inserito dall'utente non è un numero
+            alert('Il dato inserito non è un numero!!!');
+        }
+        else if(saveUserNum.includes(numUser)){
+            //il numero inserito dall'utente era già stato scritto in precedenza
+            alert('Il numero "' + numUser + '" è già stato scritto in precedenza');
+        }
+        else if(!checkRange(bombeMin,bombeMax,numUser)){
+            //il numero inserito dall'utente non è compreso tra i numeri minimi e massimi
+            alert('Il numero ' + numUser + ' è fuori dai limiti imposti. Devi usare un numero tra ' + bombeMin + ' e ' + bombeMax)
+        }
+        else {
+            //l'utente ha "preso una bomba" e quindi ha perso
+            alert('Hai perso');
+            endGame = true;
+        }
+    }
+    else {
+        // l'utente ha vinto
+    }
+});
+
 
 while(saveUserNum.length < bombeMax - numeroBombe && !endGame){
     var numUser = parseInt(prompt('Inserisci un numero tra ' + bombeMin + ' e ' + bombeMax));
@@ -86,6 +129,7 @@ console.log(saveUserNum);
 
 // 3. Comunicare quanti numeri sono stati scritti dall'utente prima di perdere
 console.log('l\'utente è riuscito ad inserire ' + saveUserNum.length + ' numeri. La percentuale di vincita è pari a ' + numPercentage(bombeMax - numeroBombe,saveUserNum.length) + '%');
+
 
 
 
